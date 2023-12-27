dc543a5a9050c9d0ac8182e015ee11d53d4abd5c
koki <kokivanov@gmail.com>
2023-12-28 00:21:47 +0200

    Added slash command example

--- src/main.py
+++ src/main.py
@@ -1,17 +1,27 @@
-import discord
+from discord import Intents, Interaction
 from discord.ext import commands
 from env import CONFIG_PATH
 
 from services import GlobalConfigService
 
-intents = discord.Intents.all()
-intents.message_content = True
-bot = commands.Bot(command_prefix='>', intents=intents)
+bot = commands.Bot(command_prefix='>', intents=Intents.all())
 config = GlobalConfigService(CONFIG_PATH)
 
+@bot.event
+async def on_ready():
+    try:
+        synced = await bot.tree.sync()
+        print(f'Synced {len(synced)} commands')
+    except Exception as e:
+        print(e)
 
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
+
+@bot.tree.command(name='test')
+async def test(interaction : Interaction):
+    await interaction.response.send_message('test')
+
 
 bot.run(config.get_bot_token())
